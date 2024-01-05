edf9a6e50dd786eac1fd29cade48e2402f86cb9c
Amit Kumar <amitkumar@adobe.com>
2024-01-05 15:12:04 +0530

    update mapping file

--- src/utils/mappings.ts
+++ src/utils/mappings.ts
@@ -12,7 +12,13 @@ governing permissions and limitations under the License.
 import { mappings } from "@aemforms/af-react-vanilla-components";
 
 const customMappings: any = {
-  ...mappings
+  ...mappings,
+  "forms-components-examples/components/form/wizard": mappings["core/fd/components/form/wizard/v1/wizard"],
+  "forms-components-examples/components/form/accordion": mappings["core/fd/components/form/accordion/v1/accordion"],
+  "forms-components-examples/components/form/tabsontop": mappings["core/fd/components/form/tabsontop/v1/tabsontop"],
+  "forms-components-examples/components/form/verticaltabs": mappings["core/fd/components/form/verticaltabs/v1/verticaltabs"],
+  "forms-components-examples/components/form/panelcontainer": mappings["core/fd/components/form/panelcontainer/v1/panelcontainer"]
+
 };
 
 export default customMappings;
