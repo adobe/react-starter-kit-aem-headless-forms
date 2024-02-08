c560f30d4cdecc7a303a246387c22f56c40670ac
Vaibhav Varshney <vaibhav1313@gmail.com>
2024-02-08 10:59:32 +0530

    Forms Demo2.0 Headless Story Usecase

--- src/Form.tsx
+++ src/Form.tsx
@@ -12,10 +12,9 @@ governing permissions and limitations under the License.
 import React, {useEffect, useState} from "react";
 import {AdaptiveForm} from "@aemforms/af-react-renderer";
 import customMappings from './utils/mappings';
-import ReactDOM from "react-dom";
 import {Action} from "@aemforms/af-core";
 import localFormJson from '../form-definitions/form-model.json';
-import '@aemforms/af-canvas-theme/dist/theme.css';
+import '@aemforms/af-wknd-theme/dist/theme.css';
 import {FunctionRuntime} from '@aemforms/af-core';
 import * as customfunctions from './utils/customfunctions';
 
@@ -75,9 +74,8 @@ const Form = (props: any) => {
     if (form != "") {
         // Register all custom functions with FunctionRuntime
         FunctionRuntime.registerFunctions({ ...customfunctions });
-        const element = document.querySelector(".cmp-formcontainer__content")
-        const retVal = (<AdaptiveForm formJson={JSON.parse(form)} mappings={customMappings} onInitialize={onInitialize} onFieldChanged={onFieldChanged} onSubmitSuccess={onSubmitSuccess} onSubmitError={onSubmitError} onSubmitFailure={onSubmitError}/>)
-        return ReactDOM.createPortal(retVal, element)
+      //  const element = document.querySelector(".cmp-formcontainer__content")
+        return <AdaptiveForm formJson={JSON.parse(form)} mappings={customMappings}/>;
     }
     return null
 }
--- src/utils/mappings.ts
+++ src/utils/mappings.ts
@@ -13,12 +13,8 @@ import { mappings } from "@aemforms/af-react-vanilla-components";
 
 const customMappings: any = {
   ...mappings,
-  "forms-components-examples/components/form/wizard": mappings["core/fd/components/form/wizard/v1/wizard"],
-  "forms-components-examples/components/form/accordion": mappings["core/fd/components/form/accordion/v1/accordion"],
-  "forms-components-examples/components/form/tabsontop": mappings["core/fd/components/form/tabsontop/v1/tabsontop"],
-  "forms-components-examples/components/form/verticaltabs": mappings["core/fd/components/form/verticaltabs/v1/verticaltabs"],
-  "forms-components-examples/components/form/panelcontainer": mappings["core/fd/components/form/panelcontainer/v1/panelcontainer"]
-
+  "aemreferencedemosharedprogram/components/adaptiveForm/verticaltabs": mappings['core/fd/components/form/verticaltabs/v1/verticaltabs'],
+  "aemreferencedemosharedprogram/components/adaptiveForm/accordion":mappings['core/fd/components/form/accordion/v1/accordion'],
 };
 
 export default customMappings;
